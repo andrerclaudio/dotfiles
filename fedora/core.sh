4703839efd38dbb5cd22d6fbbaf7b455fa9804ca
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2025-08-28 15:11:40 -0300

    Minor adjust.

--- fedora/core.sh
+++ fedora/core.sh
@@ -64,7 +64,7 @@ add_rpm_fusion_repository() {
     sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
     sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
-    sudo dnf5 group install -y core
+    sudo dnf group install -y core
 }
 
 # Function to configure GIT credentials
